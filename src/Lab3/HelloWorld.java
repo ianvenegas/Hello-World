a2886fa5aef07c46e265c4262c37acd8331f36cf
Ian Venegas <ian.venegas@pcc.edu>
2022-10-24 10:25:21 -0700

    Print out Date

--- src/Lab3/HelloWorld.java
+++ src/Lab3/HelloWorld.java
@@ -1,7 +1,10 @@
 package Lab3;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println(new Date());
     }
 }
