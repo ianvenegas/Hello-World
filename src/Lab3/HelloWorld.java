8d52ead5a0c90f3854e36912d88e070b5c36b80d
Ian Venegas <ian.venegas@pcc.edu>
2022-10-24 10:39:15 -0700

    Delete again

--- src/Lab3/HelloWorld.java
+++ src/Lab3/HelloWorld.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        System.out.println(new Date());
+        
     }
 }
